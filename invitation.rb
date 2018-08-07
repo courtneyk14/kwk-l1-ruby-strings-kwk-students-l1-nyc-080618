9229f0437394a3a70ff4b000f790a31af7f0c494
courtneyk14 <cbrookelley14@gmail.com>
2018-08-07 13:20:03 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -4,4 +4,7 @@ party_name = "You are cordially invited to the Best Halloween Party Ever"
 date= "on October 31 at 6pm. Please RSVP no later than October 30"
 host_name="Sincerely, Harry Potter"
 end
-# Try starting out with puts'ing a string.
+puts guest_name
+puts party_name
+puts date
+puts host_name
