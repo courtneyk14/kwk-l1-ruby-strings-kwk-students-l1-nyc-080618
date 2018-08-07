79da47ca6da38eb38a4f54a7788f57c33817a792
courtneyk14 <cbrookelley14@gmail.com>
2018-08-07 13:27:57 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,11 +2,13 @@ def invitation
 guest_name= "Dear Hermione"
 party_name = "You are cordially invited to the Best Halloween Party Ever"
 date = "on October 31 at 6pm. Please RSVP no later than October 30"
+time= "at 6pm. Please RSVP no later than October 30"
 host_name ="Sincerely, Harry Potter"
 
 puts guest_name
 puts party_name
 puts date
+puts time
 puts host_name
 end
 
